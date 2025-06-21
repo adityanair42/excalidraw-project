d217eeb43e2e856919a50c2678a35ece8c0acffc
adityanair42 <adityamnair42@gmail.com>
2025-06-21 10:54:13 +0000

    fix common package import in http-be

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "repo/common/types";
+import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types";
 const app = express();
 
 app.post("/signup", (req, res) => {
